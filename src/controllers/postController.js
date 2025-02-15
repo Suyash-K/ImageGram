30c7d430c33a37ff7c87dbe3a7a640ad4a1fbdf3
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-02-15 20:17:34 +0530

    add middleware

---
+++ src/controllers/postController.js
@@ -0,0 +1,10 @@
+export const createPost = async(req,res) => {
+    //call service layer function
+    return res.json({message: "Post created"}); 
+};
+
+
+// export const createPost = async (req, res) => {
+//     //call service layer function
+//     return res.json({message: "Post created"}); 
+// };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import { createPost } from './controllers/postController.js';
+
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req,res)=>{
     return res.send('HOME PAGE');
@@ -12,6 +17,22 @@ app.get('/ping', (req,res)=>{
     return res.json({message: 'pong'});
 })
 
+function m1(req,res,next){
+    console.log('m1');
+    next();
+}
+
+function m2(req,res,next){
+    console.log('m2');
+    next();
+}
+
+function m3(req,res,next){
+    console.log('m3');
+    next();
+}
+
+app.post('/post',m1,m2,m3, createPost);
 
 
 app.listen(PORT, () => {
