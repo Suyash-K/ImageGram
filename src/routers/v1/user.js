f8230ca3c8c8acb263b97a6060dd42d71a4df0a3
Suyash Kawathalkar <suyashkawathalkar@gmail.com>
2025-02-25 00:09:13 +0530

    updated

--- src/routers/v1/user.js
+++ src/routers/v1/user.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { multerUploads, dataUri } from '../../config/multerConfig.js';
 import { createPostService, findAllPosts } from '../../controllers/postController.js';
 import { cloudinaryConfig, uploader } from '../../config/cloudinaryConfig.js';
-import { getProfile, signup } from '../../controllers/userController.js';
+import { getProfile,signin, signup } from '../../controllers/userController.js';
 import { zodSignupSchema } from '../../validators/zodSignupSchema.js';
+import { zodSigninSchema } from '../../validators/zodSigninSchema.js';
 import { validate } from '../../validators/zodValidator.js';
 
 const router = express.Router();
 
 router.get('/profile',getProfile);
 router.post('/signup', validate(zodSignupSchema), signup);
+router.post('/signin', validate(zodSigninSchema), signin);
 
 export default router;
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import { createUser } from "../repositories/userRepository.js"
+import { createUser, findUserByEmail } from "../repositories/userRepository.js"
+import bcrypt from "bcrypt";
+import { generateJwtToken } from "../utils/jwtUtils.js";
 
 export const signupUserService = async (user) => {
     try {
@@ -13,6 +15,35 @@ export const signupUserService = async (user) => {
                 message: "User with the same email or username already exists"
             }
         }
+        throw error;
+    }
+}
+
+export const signinUserService = async (userDetails) => {
+    try {
+        const user = await findUserByEmail(userDetails.email);
+        if(!user) {
+            throw {
+                status: 404,
+                message: "User not found"
+            }
+        }
+
+        const isPasswordValid = bcrypt.compareSync(userDetails.password, user.password);
+
+        if(!isPasswordValid) {
+            throw {
+                status: 401,
+                message: "Invalid password"
+            }
+        }
+
+        const token = generateJwtToken({ email: user.email, _id: user._id, username: user.username });
+
+        return token;
+
+
+    } catch (error) {
         throw error;
     }
 }
